1002e34d76c2204571f37c4df18ab39e889ce4f8
Ben Summers <ben.summers@gmail.com>
2015-06-07 14:53:29 -0500

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,16 @@
 'use strict';
 $(document).ready(function(){
   $('.bxslider').bxSlider();
+
+  $('.beer').each(function() {
+    $(this).tooltipster({
+      position: 'right',
+      content: $(".info", this).html(),
+      contentAsHTML: true,
+      theme: 'cranetip',
+      interactive: true
+
+    });
+  });
+        
 });
