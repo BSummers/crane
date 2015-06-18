2fe85e2b392e0aab96b63596a04a66110d88543e
Ben Summers <ben.summers@gmail.com>
2015-06-18 09:26:16 -0500

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,5 +12,18 @@ $(document).ready(function(){
 
     });
   });
+
+$("#untappd .content").rss("https://untappd.com/rss/brewery/123025", {
+  limit: 10,
+  ssl: true,
+  tokens: {
+    link: function(entry, tokens){ console.log(entry); return entry.link },
+    content: function(entry, tokens){ return entry.content }
+  },
+
+  layoutTemplate: '<ul class="untappd-feed">{entries}</ul>',
+  entryTemplate: '<li><a href="{link}"><div>{title} {content}</div></a></li>'
+}).show();
+
         
 });
